2815298a1cd6bba4dd30c525ab7bb345152af761
lucaromagnoli <romagnoli.luca@gmail.com>
2024-10-04 19:04:32 +0100

    change page title

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { Container } from 'react-bootstrap';
 import InfiniteScrollPage from './pages/InfiniteScrollPage';
 import LoadMorePage from './pages/LoadMorePage';
